39e1ab8f66637ff3bca3a6e9ca6f772995b29fbf
hjasani-invn <hjasani@invcorp.invensense.com>
2023-03-16 12:31:44 -0600

    added bat file command

--- Survey_Dashboard/run_survey_dashboard_automation.bat
+++ Survey_Dashboard/run_survey_dashboard_automation.bat
@@ -1,3 +1,3 @@
 REM pytest -s -v .\tests\run_test_suite_1.py --browser chrome --html=.\Survey_Dashboard.html
 REM python -m pytest -v .\tests\run_test_suite_1.py --disable-warnings --browser chrome --html=.\Survey_Dashboard.html
-python -m pytest -v .\tests\run_test_suite_1.py --browser chrome --html=.\Survey_Dashboard.html
+python -m pytest -s -v .\tests\run_test_suite_1.py --browser chrome --html=.\Survey_Dashboard.html
