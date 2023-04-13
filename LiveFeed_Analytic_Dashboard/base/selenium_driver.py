6b38566dca28e4e893d6ffcee5506fa0a0d6d96a
hjasani-invn <hjasani@invcorp.invensense.com>
2023-04-13 14:15:56 -0600

    pip freeze requiremnt file

--- LiveFeed_Analytic_Dashboard/base/selenium_driver.py
+++ LiveFeed_Analytic_Dashboard/base/selenium_driver.py
@@ -11,7 +11,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import *
 import utilities.custom_logger as cl
 import logging
-import allure
 from pytest_html_reporter import attach
 
 
